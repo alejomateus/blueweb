fe729e697e56dd7674134f49453cc3a43b9750c3
Alejandro Mateus <alejo.mateus.ud@gmail.com>
2018-11-04 11:37:04 -0500

    Understanding route managment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
+import { ContactComponent } from './contact/contact.component';
+
+const routes : Routes =[
+  {path:"contact",component: ContactComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]

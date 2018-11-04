aca30443d1343e6cd2a76362b2e4d95462b87d68
Alejandro Mateus <alejo.mateus.ud@gmail.com>
2018-11-04 14:56:32 -0500

    Routes and RouterLink Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,33 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { UsComponent } from './us/us.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes : Routes =[
-  {path:"contact",component: ContactComponent}
+{path:"contact",component: ContactComponent},
+{path:"us",component: UsComponent},
+{path:"home",component: HomeComponent},
+{ path: '',
+pathMatch: 'full',
+component: HomeComponent
+},
+  { path: '**', component: PageNotFoundComponent }
 ]
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    ContactComponent
+  AppComponent,
+  HeaderComponent,
+  FooterComponent,
+  ContactComponent,
+  HomeComponent,
+  UsComponent,
+  PageNotFoundComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes)
+  BrowserModule,
+  RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
